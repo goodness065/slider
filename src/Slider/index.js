41330c6924345e1348921b6af3ae42fc433db2f2
goodness065 <goodnesslivingstone@gmail.com>
2022-05-20 01:34:08 +0100

    first commit

---
+++ src/Slider/index.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "../data";
+
+const Slide = () => {
+  const [index, setIndex] = useState(0);
+  const people = data;
+
+  const checkIndex = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex((oldIndex) => {
+        let index = oldIndex + 1;
+        if (index > people.length - 1) {
+          index = 0;
+        }
+        return index;
+      });
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index, people.length]);
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button
+          className="prev"
+          onClick={() => {
+            let newNumber = index - 1;
+            setIndex(checkIndex(newNumber));
+          }}
+        >
+          <FiChevronLeft />
+        </button>
+        <button
+          className="next"
+          onClick={() => {
+            let newNumber = index + 1;
+            setIndex(checkIndex(newNumber));
+          }}
+        >
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Slide;
